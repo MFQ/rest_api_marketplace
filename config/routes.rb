dce6420518747a8ced6cd7cd3faf823040ca0518
MFQ <softmfq@gmail.com>
2015-02-23 03:03:06 +0500

    routes are added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
+require 'api_contraints'
 Rails.application.routes.draw do
   #APi namespace 
 
   namespace :api, defaults:{ format: :json}, constraints: {subdomain: 'api'}, path: '/' do
-  # We are going to list our resources here
+  
+    scope module: :v1, constraints: ApiContraints.new(version: 1, defaults: true) do
+      # We are going to list our resources here
+    end
   end
  
 end
