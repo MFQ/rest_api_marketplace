93dcd2ee4a0f81336ab9b74ae641452b5daaa2d4
MFQ <softmfq@gmail.com>
2015-02-24 02:54:51 +0500

    rename raints'

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
-require 'api_contraints'
-Rails.application.routes.draw do
+require 'api_constraints'
+
+RestApiMarketplace::Application.routes.draw do
   #APi namespace 
 
   namespace :api, defaults:{ format: :json}, constraints: {subdomain: 'api'}, path: '/' do
   
-    scope module: :v1, constraints: ApiContraints.new(version: 1, defaults: true) do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, defaults: true) do
       # We are going to list our resources here
     end
   end
