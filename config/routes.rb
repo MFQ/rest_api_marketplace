cc9cbbec14296cb35c75e5c833ad655115ae952e
MFQ <softmfq@gmail.com>
2015-05-18 01:36:48 +0500

    orders

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ RestApiMarketplace::Application.routes.draw do
       resources :users, only:[:show, :create, :update, :destroy]
       resources :sessions, only:[:create, :destroy]
       resources :products, only:[:show, :index, :create, :update, :destroy]
+      resources :orders, only:[:index]
     end
   end
  
